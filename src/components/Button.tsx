cd2d81a101bf4a211a93b7bc1380505ddc08d89f
Mariadssantos <madudallas16@gmail.com>
2021-06-22 21:02:43 -0300

    Dia2-Autenticação

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonHTMLAttributes, HtmlHTMLAttributes } from "react";
+
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes <HTMLButtonElement>;
+
+export function Button (props: ButtonProps){
+    return(
+        <button className="button" {...props}/>
+    )
+}
+
+<Button />
---
+++ src/pages/Home.tsx
@@ -0,0 +1,55 @@
+
+import { useHistory } from 'react-router-dom';
+
+import { Button } from '../components/Button';
+
+import illustrationImg from '../assets/images/illustration.svg'
+import LogoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
+
+
+
+export function Home() {
+    const history = useHistory();
+    const {user, signInWithGoogle} = useAuth()
+
+  async function handleCreateRoom(){
+    if(!user) {
+       await signInWithGoogle()
+    }
+
+    history.push('/rooms/new');
+ } 
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+            
+            <main>
+                <div className="main-content">
+                    <img src={LogoImg} alt="Letmeask" />
+                    <button onClick={handleCreateRoom} className="create-room">
+                        <img src={googleIconImg} alt="Logo da Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div className="separator">ou entre em uma sala</div>
+                <form>
+                    <input 
+                    type="text" 
+                    placeholder="Digite o código da sala"
+                    />
+                    <Button type="submit">
+                        Entrar na sala
+                    </Button>
+                </form>
+                </div>
+            </main>
+        </div>
+    )
+}
